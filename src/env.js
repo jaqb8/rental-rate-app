8f99d5e620d587e661321724f1a0098d43f521ce
jaqb8 <jakubaniszewski@pm.me>
2024-10-20 00:03:37 +0200

    feat: update APP_URL handling for Vercel deployments; use VERCEL_PROJECT_PRODUCTION_URL for URL formatting

--- src/env.js
+++ src/env.js
@@ -25,7 +25,9 @@ export const env = createEnv({
     APP_URL: z.preprocess(
       // This makes Vercel deployments not fail if you don't set APP_URL
       (str) =>
-        process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : str,
+        process.env.VERCEL_PROJECT_PRODUCTION_URL
+          ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+          : str,
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
       process.env.VERCEL ? z.string() : z.string().url(),
     ),
