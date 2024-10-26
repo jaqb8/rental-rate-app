58742464d723bbf489aabf73dbc9ee84d8fcb230
jaqb8 <jakubaniszewski@pm.me>
2024-10-26 18:19:08 +0200

    Refactor submit-button component and add i18n support

--- src/app/[locale]/(dashboard)/(auth)/components/submit-button.tsx
+++ src/app/[locale]/(dashboard)/(auth)/components/submit-button.tsx
@@ -4,16 +4,19 @@ import { forwardRef } from "react";
 import { useFormStatus } from "react-dom";
 import { Button, type ButtonProps } from "@/components/ui/button";
 import { Loader2 } from "lucide-react";
+import { useScopedI18n } from "locales/client";
 
 const SubmitButton = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, children, ...props }, ref) => {
     const { pending } = useFormStatus();
+    const t = useScopedI18n("Profile");
+
     return (
       <Button disabled={pending} ref={ref} {...props} className={className}>
         {pending && (
           <>
             <Loader2 className="mr-2 animate-spin" />
-            <span>Processing...</span>
+            <span>{t("processing")}</span>
           </>
         )}
         {!pending && <>{children}</>}
